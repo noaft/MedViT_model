6c6627a305e862518440b972dd00cab935614023
noaft <nvantoan1203@gmail.com>
2025-04-11 16:26:42 +0700

    add model MedViT small

---
+++ model_load.py
@@ -0,0 +1,26 @@
+import torch
+import timm
+
+def load_model(model_path, device):
+    """
+    Load a pre-trained MedViT model from the specified path.
+
+    Args:
+        model_path (str): Path to the pre-trained model file.
+        device (torch.device): Device to load the model onto (CPU or GPU).
+
+    Returns:
+        MedViT: Loaded MedViT model.
+    """
+    model = timm.create_model('MedViT_small', num_classes = 7) 
+    
+    # Load the state dictionary
+    state_dict = torch.load(model_path, map_location=device)
+    
+    # Load the state dictionary into the model
+    model.load_state_dict(state_dict)
+    
+    # Move the model to the specified device
+    model.to(device)
+    
+    return model
